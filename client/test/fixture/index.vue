f0da1753aec21f2cc3495a59112a29410b31092b
FF <pucelle@163.com>
2021-11-24 09:36:49 +0800

    Test case for #30.

--- client/test/fixture/index.vue
+++ client/test/fixture/index.vue
@@ -1,14 +1,40 @@
 <style src="./css/test.scss"></style>
 <style src="./node_modules/test-module/style-imported.scss"></style>
 
-<style scope>
+<style scoped>
     .test-vue-inner-class{
         color: red;
     }
 </style>
 
+<style scoped lang="css">
+    .test-vue-inner-class-css{
+        color: red;
+    }
+</style>
+
+<style scoped lang="scss">
+    .test-vue-inner-class{
+        &-scss{
+            color: red;
+        }
+    }
+</style>
+
+<style scoped lang="less">
+    .test-vue-inner-class{
+        &-less{
+            color: red;
+        }
+    }
+</style>
+
 <template>
     <div class="test-vue-inner-class">Test Text</div>
+    <div class="test-vue-inner-class-css">Test Text</div>
+    <div class="test-vue-inner-class-scss">Test Text</div>
+    <div class="test-vue-inner-class-less">Test Text</div>
+
     <div class="test-vue-import-class">Test Text</div>
     <div class="class-style-imported">Test Text</div>
 </template>
